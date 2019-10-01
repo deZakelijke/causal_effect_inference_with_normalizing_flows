b99728dd42b43d3cd39115ac20027314e0a20515
Micha de Groot <git@michadegroot.nl>
2019-10-01 16:10:32 +0200

    Really not sure what im doing

--- src/cevae.py
+++ src/cevae.py
@@ -1,7 +1,10 @@
 import numpy as np
-import torch
-from torch import nn
-import torchvision
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from tensorflow.keras.activations import softplus
+from tensorflow_probability import distributions as tfd
+from fc_net import FC_net
+
 
 """
 Old structure:
@@ -80,9 +83,52 @@ Old structure:
 
 """
 
-class CEVAE(nn.module):
+
+def make_cevae(x_bin_size, x_cont_size, z_size, hidden_size=512):
     """ CEVAE model with fc nets between random variables.
+    https://github.com/tensorflow/probability/blob/master/tensorflow_probability/examples/vae.py
     """
 
-    def __init__(self):
-        super.__init__()
+        # Encoder part
+        #qt       = tfd.Bernoulli(logits=FC_net(1, hidden_size), dtype=tf.float32)
+        qt_logits = FC_net(1, hidden_size)
+        hqy      = FC_net(hidden_size, hidden_size)
+        mu_qy_t0 = FC_net(1, hidden_size)
+        mu_qy_t1 = FC_net(1, hidden_size)
+        #qy       = tfd.Normal(loc=qt * mu_qy_t1 + (1. - qt) * mu_qy_t0, scale=tf.ones_like(mu_qy_t0))
+
+        hqz   = FC_net(z_size, hidden_size)
+        qz_t0 = FC_net(z_size * 2, hidden_size)
+        qz_t1 = FC_net(z_size * 2, hidden_size)
+        #qz    = tfd.Normal(loc=self.qt * qz_t1[:, :z_size] + (1. - self.qt) * qz_t0[:, :z_size],
+                scale=self.qt * softplus(qz_t1[:, z_size:]) + (1. - self.qt) * softplus(qz_t0[:, z_size:]))
+
+        # Decoder part
+        #pz = tfd.Normal(loc=tf.zeros([z_size]), scale=tf.ones([z_size]))
+        hx      = FC_net(x_bin_size + x_cont_size, hidden_size)
+        #x1      = tfd.Bernoulli(logits=FC_net(x_bin_size, hidden_size), dtype=tf.float32)
+        x_cont_logits = FC_net(x_cont_size, hidden_size)
+        hx_c    = FC_net(x_cont_size * 2, hidden_size)
+        #x2      = tfd.Normal(loc=hx_c[:, :x_cont_size], scale=softplus(hx_c[:, x_cont_size:]))
+        x_bin_logits = FC_net(x_bin_size, hidden_size)
+        #t       = tdf.Bernoulli(logits=FC_net(1, hidden_size), dtype=tf.float32)
+        t_logtis = FC_net(1, hidden_size)
+        mu_y_t0 = FC_net(1, hidden_size)
+        mu_y_t1 = FC_net(1, hidden_size)
+        #y       = tfd.Normal(loc=t * mu_y_t1 + (1. - t) * mu_y_t0, scale=tf.ones_like(mu_y_t0))
+
+
+    def encode(self, x, y, t):
+        qt = tfd.Bernoulli(qt_logits(x))
+        mu0 = mu_qy_t0(hqy(qt))
+        mu1 = mu_qy_t1(hqy(qt))
+        qy = tdf.Normal(qt * mu1 + (1. - qt) * mu0, scale-tf.ones_like(mu0))
+
+
+
+
+    return encode, decode
+
+
+
+
--- src/fc_net.py
+++ src/fc_net.py
@@ -3,17 +3,17 @@ from tensorflow.keras import layers, Model
 
 class FC_net(Model):
     """ Simple fully connected net with not activation after the last layer. """
-    def __init__(self, in_dims, out_dims, nr_hidden=1, hidden_dims=256, activation='relu'):
+    def __init__(self, out_dims, nr_hidden=3, hidden_dims=256, activation='relu'):
         super.__init__()
 
         if nr_hidden < 1:
             print("Must have at leas one hidden layer")
             return None
 
-        nn_layers = [layers.Dense(hidden_dims), activation=activation)]
+        nn_layers = [layers.Dense(hidden_dims, activation=activation)]
 
         for i in range(nr_hidden - 1):
-            new_layer = layers.Dense(hidden_dims), activation=activation)
+            new_layer = layers.Dense(hidden_dims, activation=activation)
             nn_layers.append(new_layer)
  
         nn_layers.append(nn.Linear(out_dims, out_dims))
