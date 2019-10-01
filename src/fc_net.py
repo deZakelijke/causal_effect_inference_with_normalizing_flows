72b49e3c6191e8c3e7a452cae2531c4e153c21c2
Micha de Groot <git@michadegroot.nl>
2019-10-01 10:37:56 +0200

    Rewrote fc to tensorflow

--- src/fc_net.py
+++ src/fc_net.py
@@ -1,23 +1,23 @@
-import torch
-from torch import nn
+import tensorflow as tf
+from tensorflow.keras import layers, Model
 
-class FC_net(nn.Module):
+class FC_net(Model):
     """ Simple fully connected net with not activation after the last layer. """
-    def __init__(self, in_dims, out_dims, nr_hidden=1, hidden_dims=256, activation=nn.ReLU()):
+    def __init__(self, in_dims, out_dims, nr_hidden=1, hidden_dims=256, activation='relu'):
         super.__init__()
 
         if nr_hidden < 1:
             print("Must have at leas one hidden layer")
             return None
 
-        layers = [nn.Sequential(nn.Linear(in_dims, hidden_dims), activation)]
+        nn_layers = [layers.Dense(hidden_dims), activation=activation)]
 
         for i in range(nr_hidden - 1):
-            new_layer = nn.Sequential(nn.Linear(hidden_dims, hidden_dims), activation)
-            layers.append(new_layer)
+            new_layer = layers.Dense(hidden_dims), activation=activation)
+            nn_layers.append(new_layer)
  
-        layers.append(nn.Linear(hidden_dims, out_dims))
-        self.layers = nn.Sequential(layers)
+        nn_layers.append(nn.Linear(out_dims, out_dims))
+        self.nn_layers = tf.keras.Sequential(nn_layers)
 
-    def forward(self, x):
-        return self.layers(x)
+    def call(self, x):
+        return self.nn_layers(x)
