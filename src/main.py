a5c4c8f18ac488f39e79305ed82fd5d569eec27d
Micha de Groot <git@michadegroot.nl>
2020-02-06 15:15:16 +0100

    added experiment names

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 import logging
 import os
+import re
 import sys
 import tensorflow as tf
 import time
@@ -17,6 +18,7 @@ VALID_DATASETS = ["IHDP"]
 tf.keras.backend.set_floatx('float64')
 
 def parse_arguments():
+    """ Parse all arguments and check for faulty input. """
     parser = argparse.ArgumentParser(description="Causal effect Normalizing Flow trainer")
     parser.add_argument("--batch_size", type=int, default=16, 
                         help="Batch size (default=16)")
@@ -26,6 +28,8 @@ def parse_arguments():
                         help="Turn on debugging mode. What it does now is turn off summary writer and print a lott of stuff.")
     parser.add_argument("--epochs", type=int, default=100, 
                         help="Number of training iterations (default: 100)")
+    parser.add_argument("--experiment_name", type=str, 
+                        help="Name of experiment used for results folder. Disabled in debug mode")
     parser.add_argument("--learning_rate", type=float, default=1e-4, 
                         help="Learning rate of the optmiser (default: 1e-4)")
     parser.add_argument("--log_steps", type=int, default=10, 
@@ -45,15 +49,23 @@ def parse_arguments():
 
     if args.debug:
         tf.random.set_seed(0)
+        args.experiment_name = ""
     else:
         os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
         logging.getLogger("tensorflow").setLevel(logging.WARNING)
+        if args.experiment_name is None:
+            raise ValueError("Experiment name is required if debug mode is disabled")
+        if not re.match(r'^[A-Za-z0-9_]+$', args.experiment_name):
+            raise ValueError("Experment name may only contain alphanumerical and underscore")
 
     if not args.model in VALID_MODELS:
        raise NotImplementedError(f"Model {args.model} is not implemented")
 
     if not args.dataset in VALID_DATASETS:
        raise NotImplementedError(f"Dataset {args.dataset} is not implemented")
+
+    if not args.mode == "train":
+        raise NotImplementedError("Only training mode is implemented")
 
     if args.model == "cevae":
         args.nr_flows = 0
@@ -65,13 +77,15 @@ def parse_arguments():
     return vars(args)
 
 def main(params):
+    """ Main execution. Creates logging aand writer, and launches selected training. """
     params["x_bin_size"] = 19
     params["x_cont_size"] = 6
     params["z_size"] = 16
 
     timestamp = time.strftime("%Y:%m:%d/%X")
-    logdir = f"{params['model_dir']}{params['model']}/{params['dataset']}/{params['learning_rate']}/{timestamp}"
     if not params["debug"]:
+        logdir = (f"{params['model_dir']}{params['model']}/{params['dataset']}/"
+                  f"{params['learning_rate']}/{timestamp}/{params['experiment_name']}")
         writer = tf.summary.create_file_writer(logdir)
     else:
         writer = None
@@ -92,6 +106,7 @@ def main(params):
 
 
 def train(params, dataset, len_dataset, writer, y_mean, y_std, train_iteration=0):
+    """ Runs training of selected model. """
     if params["model"] == "cevae":
         model = CEVAE(params)
     if params["model"] == "cenf":
@@ -140,7 +155,7 @@ def train(params, dataset, len_dataset, writer, y_mean, y_std, train_iteration=0
         print(f"Epoch: {epoch}, average loss: {(avg_loss / tf.dtypes.cast(len_epoch, tf.float64)):.4f}")
         stats = calc_stats(model, dataset, y_mean, y_std, params)
         print(f"Average ite: {stats[0]:.4f}, abs ate: {stats[1]:.4f}, pehe; {stats[2]:.4f}")
-        l_step = (epoch + global_log_step) // params['log_steps']
+        l_step = (epoch + global_log_step + 1) // params['log_steps']
         tf.summary.scalar("metrics/loss", loss_value, step=l_step)
         tf.summary.scalar("metrics/ite", stats[0], step=l_step)
         tf.summary.scalar("metrics/ate", stats[1], step=l_step)
@@ -155,6 +170,7 @@ if __name__ == "__main__":
     params = parse_arguments()
 
     gpus = tf.config.experimental.list_physical_devices("GPU")
+    #tf.config.experimental_run_functions_eagerly(True)
     if gpus:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, False)
