Re-added correct normalisation, updated cenf to current architecture

src/cenf.py
@@ -38,7 +38,7 @@ class CENF(Model):
         x_bin, x_cont, t, y, y_cf, mu_0, mu_1 = features
         x = tf.concat([x_bin, x_cont], 1)
 
-        encoder_params = self.encode(x, step, training=training)
+        encoder_params = self.encode(x, t, y, step, training=training)
         _, _, qz_mean, qz_std = encoder_params
         qz = tfd.Independent(tfd.Normal(loc=qz_mean, scale=qz_std),
                              reinterpreted_batch_ndims=1,
@@ -56,38 +56,39 @@ class CENF(Model):
             print("Calculating loss")
         x_bin, x_cont, t, y, y_cf, mu_0, mu_1 = features
         qt_prob, qy_mean, qz_mean, qz_std = encoder_params
-        x_bin_prob, x_cont_mean, x_cont_std, t_prob, y_mean = decoder_params
+        x_bin_prob, x_cont_mean, x_cont_std, t_prob, t_sample, y_mean = decoder_params
 
+        # Get the y values corresponding to the values of t that were sampled during decoding
+        t_correct = tf.where(tf.squeeze(t) == tf.squeeze(t_sample))
+        t_incorrect = tf.where(tf.squeeze(t) != tf.squeeze(t_sample))
+        y_labels = tf.scatter_nd(t_correct, tf.squeeze(tf.gather(y, t_correct), axis=2), y.shape) + \
+                   tf.scatter_nd(t_incorrect, tf.squeeze(tf.gather(y_cf, t_incorrect), axis=2), y.shape)
+
+
+        distortion_x = -get_log_prob(x_bin, 'B', probs=x_bin_prob) \
+                       -get_log_prob(x_cont, 'N', mean=x_cont_mean, std=x_cont_std)
+        distortion_t = -get_log_prob(t, 'B', probs=t_prob)
+        distortion_y = -get_log_prob(y_labels, 'N', mean=y_mean)
+
+        rate = get_analytical_KL_divergence(qz_mean, qz_std)
         ldj = tf.reduce_mean(-ldj)
 
-        if self.debug:
-            print("Calculating negative data log likelihood")
-        distortion_x = tf.reduce_mean(- get_log_prob(x_bin, 'B', probs=x_bin_prob) \
-                           - get_log_prob(x_cont, 'N', mean=x_cont_mean, std=x_cont_std))
-        distortion_t = tf.reduce_mean(- get_log_prob(t, 'B', probs=t_prob))
-        distortion_y = tf.reduce_mean(- get_log_prob(y, 'N', mean=y_mean))
+        variational_t = -get_log_prob(t, 'B', probs=qt_prob)
+        variational_y = -get_log_prob(y, 'N', mean=qy_mean)
 
-        if self.debug:
-            print("Calculating KL-divergence")
-        rate = tf.reduce_mean(get_analytical_KL_divergence(qz_mean, qz_std))
-
-        if self.debug:
-            print("Calculating negative log likelihood of auxillary distributions")
-        variational_t = - tf.reduce_mean(get_log_prob(t, 'B', probs=qt_prob))
-        variational_y = - tf.reduce_mean(get_log_prob(y, 'N', mean=qy_mean))
-
-        if step % (params['log_steps'] * 5) == 0:
+        if not step is None and step % (params['log_steps'] * 5) == 0:
             l_step = step // (params['log_steps'] * 5)
-            tf.summary.scalar("partial_loss/distortion_x", distortion_x, step=l_step)
-            tf.summary.scalar("partial_loss/distortion_t", distortion_t, step=l_step)
-            tf.summary.scalar("partial_loss/distortion_y", distortion_y, step=l_step)
-            tf.summary.scalar("partial_loss/rate_z", rate, step=l_step)
+            tf.summary.scalar("partial_loss/distortion_x", tf.reduce_mean(distortion_x), step=l_step)
+            tf.summary.scalar("partial_loss/distortion_t", tf.reduce_mean(distortion_t), step=l_step)
+            tf.summary.scalar("partial_loss/distortion_y", tf.reduce_mean(distortion_y), step=l_step)
+            tf.summary.scalar("partial_loss/rate_z", tf.reduce_mean(rate), step=l_step)
             tf.summary.scalar("partial_loss/ldj", ldj, step=l_step)
-            tf.summary.scalar("partial_loss/variational_t", variational_t, step=l_step)
-            tf.summary.scalar("partial_loss/variational_y", variational_y, step=l_step)
+            tf.summary.scalar("partial_loss/variational_t", tf.reduce_mean(variational_t), step=l_step)
+            tf.summary.scalar("partial_loss/variational_y", tf.reduce_mean(variational_y), step=l_step)
 
 
-        elbo_local = -(rate + distortion_x + distortion_t + distortion_y + 
+
+        elbo_local = -(rate + distortion_x + distortion_t + distortion_y + \
                        variational_t + variational_y + ldj)
         elbo = tf.reduce_mean(input_tensor=elbo_local)
         return -elbo
@@ -100,4 +101,15 @@ class CENF(Model):
             print(f"Forward pass complete, step: {step}")
         return loss, tape.gradient(loss, self.trainable_variables)
 
+    def do_intervention(self, x, nr_samples):
+        _, _, qz_mean, qz_std = self.encode(x, None, None, None, training=False)
+        qz = tfd.Independent(tfd.Normal(loc=qz_mean, scale=qz_std),
+                             reinterpreted_batch_ndims=1,
+                             name="qz")
+        qz_sample = qz.sample(nr_samples)
+
+        qz_k, ldj = self.planar_flow(qz_sample, None, training=False)
+
+        mu_y0, mu_y1 = self.decode.do_intervention(qz_k, nr_samples)
+        return mu_y0, mu_y1
 

src/cevae.py
@@ -57,7 +57,7 @@ class CEVAE(Model):
         decoder_params = self.decode(qz_sample, step, training=training)
         return encoder_params, decoder_params
 
-    #@tf.function
+    @tf.function
     def elbo(self, features, encoder_params, decoder_params, step, params):
         if self.debug:
             print("Calculating loss")
@@ -75,10 +75,6 @@ class CEVAE(Model):
                        -get_log_prob(x_cont, 'N', mean=x_cont_mean, std=x_cont_std)
         distortion_t = -get_log_prob(t, 'B', probs=t_prob)
         distortion_y = -get_log_prob(y_labels, 'N', mean=y_mean)
-        # TODO here we should use y of the values that actually got chosen
-        # if t~p(t|z) is not the same as the actual t then we take the wrong loss
-        #print(f"y labels: {y_labels[:10]}")
-        #print(f"y mean: {y_mean[:10]}")
 
         rate = get_analytical_KL_divergence(qz_mean, qz_std)
 
@@ -94,9 +90,8 @@ class CEVAE(Model):
             tf.summary.scalar("partial_loss/variational_t", tf.reduce_mean(variational_t), step=l_step)
             tf.summary.scalar("partial_loss/variational_y", tf.reduce_mean(variational_y), step=l_step)
 
-        # Either pick beta to increase rate or decrease only dist of x
-        #elbo_local = -(params['beta'] * rate + distortion_x + distortion_t + distortion_y + variational_t + variational_y)
-        elbo_local = -(params['beta'] * rate + distortion_x + distortion_t + distortion_y + variational_t + variational_y)
+        elbo_local = -(params['beta'] * rate + distortion_x + distortion_t + \
+                       distortion_y + variational_t + variational_y)
         elbo = tf.reduce_mean(elbo_local)
 
         return -elbo
@@ -111,7 +106,13 @@ class CEVAE(Model):
 
     def do_intervention(self, x, nr_samples):
         _, _, qz_mean, qz_std = self.encode(x, None, None, None, training=False)
-        mu_y0, mu_y1 = self.decode.do_intervention(qz_mean, qz_std, nr_samples)
+
+        qz = tfd.Independent(tfd.Normal(loc=qz_mean, scale=qz_std),
+                             reinterpreted_batch_ndims=1,
+                             name="qz")
+        z = qz.sample(nr_samples)
+
+        mu_y0, mu_y1 = self.decode.do_intervention(z, nr_samples)
         return mu_y0, mu_y1
 
 
@@ -157,17 +158,6 @@ class Encoder(Model):
             qy_mean = t * mu_qy1 + (1. - t) * mu_qy0
         else:
             qy_mean = qt_sample * mu_qy1 + (1. - qt_sample) * mu_qy0
-
-        #if tf.math.reduce_any(tf.math.is_nan(qy_mean)):
-        # This doesnt work if call() is a compiled TF function
-        #    print("hit")
-        #    print("qy", qy_mean)
-        #    print("qy0", mu_qy0)
-        #    print("qy1", mu_qy1)
-        #    print("hqy", hqy)
-        #    print("qt_prob", qt_prob)
-        #    print("x", x)
-        #    print("test\n\n")
 
         qy = tfd.Independent(tfd.Normal(loc=qy_mean, scale=tf.ones_like(qy_mean)),
                              reinterpreted_batch_ndims=1,
@@ -238,7 +228,7 @@ class Decoder(Model):
         y_mean = t_sample * mu_y1 + (1. - t_sample) * mu_y0
         return x_bin_prob, x_cont_mean, x_cont_std, t_prob, t_sample, y_mean
 
-    def do_intervention(self, qz_mean, qz_std, nr_samples):
+    def do_intervention(self, z, nr_samples):
         """ Computes the quantity E[y|z, do(t=0)] and E[y|z, do(t=1)]
 
     `   nr_sample of samples are drawn from the Normal distribution
@@ -246,11 +236,6 @@ class Decoder(Model):
         The samples are averaged at the end.
 
         """
-        qz = tfd.Independent(tfd.Normal(loc=qz_mean, scale=qz_std),
-                             reinterpreted_batch_ndims=1,
-                             name="qz")
-        z = qz.sample(nr_samples)
-
         y0 = self.mu_y_t0(z, None, training=False)
         y1 = self.mu_y_t1(z, None, training=False)
         mu_y0 = tf.reduce_mean(y0, axis=0)

src/dataset.py
@@ -22,8 +22,8 @@ def IHDP_dataset(params, path_data="datasets/IHDP/csv/", file_prefix="ihdp_npci_
             t.append(line[0])       # Treatment true/false
             y.append(line[1])       # Outcome
             y_cf.append(line[2])    # Counterfactual outcome
-            mu_0.append(line[3])    # Still not sure what these are 
-            mu_1.append(line[4])    # ?????
+            mu_0.append(line[3])    # Outcome of the experiment if the dataset had been created with
+            mu_1.append(line[4])    # a randomised double lbind trial
             x = line[5:]            # Proxy features
             x[13] -= 1              # Value is in [1, 2] instead of [0, 1]
             x_bin.append(x[binfeats])
@@ -45,15 +45,16 @@ def IHDP_dataset(params, path_data="datasets/IHDP/csv/", file_prefix="ihdp_npci_
     x_cont = np.array(x_cont)
     x_bin = np.array(x_bin)
     t = np.expand_dims(np.array(t), axis=1)
-    y = np.expand_dims(np.array(y), axis=1) # Y needs to have zero mean and std 1 during training
-    y_mean, y_std = np.mean(y), np.std(y)
-    #y = (y - y_mean) / y_std
+    y = np.expand_dims(np.array(y), axis=1) 
     y_cf = np.expand_dims(np.array(y_cf), axis=1)
-    y_cf_mean, y_cf_std = np.mean(y_cf), np.std(y_cf)
-    #y_cf = (y_cf - y_cf_mean) / y_cf_std
+
+    y_mean, y_std = np.mean(tf.concat([y, y_cf], 1)), np.std(tf.concat([y, y_cf], 1))
+    y = (y - y_mean) / y_std
+    y_cf = (y_cf - y_mean) / y_std
+
     mu_0 = np.expand_dims(np.array(mu_0), axis=1)
     mu_1 = np.expand_dims(np.array(mu_1), axis=1)
-    scaling_data = (y_mean, y_std, y_cf_mean, y_cf_std)
+    scaling_data = (y_mean, y_std)
 
     return tf.data.Dataset.from_tensor_slices(((x_bin, 
                                                 x_cont, 

src/main.py
@@ -82,11 +82,22 @@ def parse_arguments():
 
     return vars(args)
 
+def print_stats(stats, index):
+        print(f"Average ite: {stats[0]:.4f}, abs ate: {stats[1]:.4f}, pehe; {stats[2]:.4f}, "\
+              f"y_error factual: {stats[3][0]:.4f}, y_error counterfactual {stats[3][1]:.4f}")
+ 
+        tf.summary.scalar("metrics/ite", stats[0], step=index)
+        tf.summary.scalar("metrics/ate", stats[1], step=index)
+        tf.summary.scalar("metrics/pehe", stats[2], step=index)
+        tf.summary.scalar("metrics/y_factual", stats[3][0], step=index)
+        tf.summary.scalar("metrics/y_counterfactual", stats[3][1], step=index)
+
 def main(params):
     """ Main execution. Creates logging aand writer, and launches selected training. """
     params["x_bin_size"] = 19
     params["x_cont_size"] = 6
     params["z_size"] = 16
+    repetitions = 10
 
     timestamp = time.strftime("%Y:%m:%d/%X")
     if not params["debug"]:
@@ -98,13 +109,16 @@ def main(params):
 
 
     if params["separate_files"]:
-        for i in range(10):
+        total_stats = []
+        for i in range(repetitions):
             dataset, scaling_data = eval(f"{params['dataset']}_dataset")(params, separate_files=True, file_index=i)
             len_dataset = tf.data.experimental.cardinality(dataset)
             dataset = dataset.shuffle(len_dataset)
-            train(params, dataset, len_dataset, writer, scaling_data, i)
-            if params['debug']:
-                break
+            stats = train(params, dataset, len_dataset, writer, scaling_data, i)
+            total_stats.append(stats)
+        total_stats = np.array(total_stats)
+        print_stats(total_stats.mean(0), repetitions * params['log_steps'] + 1)
+        
     else:
         dataset, scaling_data = eval(f"{params['dataset']}_dataset")(params)
         len_dataset = tf.data.experimental.cardinality(dataset)
@@ -139,7 +153,7 @@ def train(params, dataset, len_dataset, writer, scaling_data, train_iteration=0)
             stats = calc_stats(model, dataset, scaling_data, params)
             print(f"Average ite: {stats[0]:.4f}, abs ate: {stats[1]:.4f}, pehe; {stats[2]:.4f}, y_error factual: {stats[3][0]:.4f}, y_error counterfactual {stats[3][1]:.4f}")
             print("Epoch done")
-        return
+        return stats
 
 
     with writer.as_default():
@@ -153,30 +167,16 @@ def train(params, dataset, len_dataset, writer, scaling_data, train_iteration=0)
             if epoch % params["log_steps"] == 0:
                 print(f"Epoch: {epoch}, average loss: {(avg_loss / tf.dtypes.cast(len_epoch, tf.float64)):.4f}")
                 stats = calc_stats(model, dataset, scaling_data, params)
-                print(f"Average ite: {stats[0]:.4f}, abs ate: {stats[1]:.4f}, pehe; {stats[2]:.4f}, "\
-                      f"y_error factual: {stats[3][0]:.4f}, y_error counterfactual {stats[3][1]:.4f}")
- 
                 l_step = (epoch + global_log_step) // params['log_steps']
-                tf.summary.scalar("metrics/loss", avg_loss / tf.dtypes.cast(len_epoch, tf.float64), step=l_step)
-                tf.summary.scalar("metrics/ite", stats[0], step=l_step)
-                tf.summary.scalar("metrics/ate", stats[1], step=l_step)
-                tf.summary.scalar("metrics/pehe", stats[2], step=l_step)
-                tf.summary.scalar("metrics/y_factual", stats[3][0], step=l_step)
-                tf.summary.scalar("metrics/y_counterfactual", stats[3][1], step=l_step)
+                print_stats(stats, l_step)
+                tf.summary.scalar("metrics/loss", loss_value, step=l_step)
 
         print(f"Epoch: {epoch}, average loss: {(avg_loss / tf.dtypes.cast(len_epoch, tf.float64)):.4f}")
         stats = calc_stats(model, dataset, scaling_data, params)
-        print(f"Average ite: {stats[0]:.4f}, abs ate: {stats[1]:.4f}, pehe; {stats[2]:.4f}, "\
-              f"y_error factual: {stats[3][0]:.4f}, y_error counterfactual {stats[3][1]:.4f}")
- 
         l_step = (epoch + global_log_step + 1) // params['log_steps']
+        print_stats(stats, l_step)
         tf.summary.scalar("metrics/loss", loss_value, step=l_step)
-        tf.summary.scalar("metrics/ite", stats[0], step=l_step)
-        tf.summary.scalar("metrics/ate", stats[1], step=l_step)
-        tf.summary.scalar("metrics/pehe", stats[2], step=l_step)
-        tf.summary.scalar("metrics/y_factual", stats[3][0], step=l_step)
-        tf.summary.scalar("metrics/y_counterfactual", stats[3][1], step=l_step)
-
+        return stats
 
 
 def test(params):