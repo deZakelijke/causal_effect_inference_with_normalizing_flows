fe40d018a35539e31904abe7e20c9e00d4eeaa61
Micha <git@michadegroot.nl>
2020-06-17 16:38:27 +0200

    Added file writing to space blocks

--- src/space_blocks.py
+++ src/space_blocks.py
@@ -12,6 +12,7 @@ parallelised massively to generate a lot of data quickly.
 
 """
 
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage
@@ -59,61 +60,71 @@ class SpaceShapesGenerator():
         self.position_map = random.standard_normal((prior_dims + num_objects,
                                                     width * height))
         self.object_gravity = random.standard_normal((1, num_objects))
+        print(f"Gravities: {self.object_gravity[0]}")
 
     def generate_data(self, nr_samples):
         prior_data = random.standard_normal((nr_samples, self.prior_dims))
-        object_gravity = np.tile(self.object_gravity, nr_samples)
+        object_gravity = np.tile(self.object_gravity, (nr_samples, 1))
         prior_data = np.concatenate((prior_data, object_gravity), axis=1)
-        position_probs = softmax(prior_data @ self.position_map)
-        position_probs = np.reshape(position_probs, (self.height, self.width))
+
+        position_probs = softmax(prior_data @ self.position_map, axis=1)
+        position_probs = np.reshape(position_probs, (nr_samples, self.height, self.width))
         object_positions = self.set_positions(position_probs)
+        rendering = np.array([self.render(objects) for objects in object_positions])
+        with h5py.File("datasets/SPACE/space_data_x.hdf5", "w") as f:
+            dset = f.create_dataset("Space_dataset_x", data=rendering)
 
         steering = prior_data @ self.intervention_map
+        with h5py.File("datasets/SPACE/space_data_t.hdf5", "w") as f:
+            dset = f.create_dataset("Space_datset_t", data=steering)
 
-        rendering = self.render(object_positions)
         plt.subplot(121)
-        plt.imshow(rendering)
+        plt.imshow(rendering[0])
         plt.title(f"Steering: {np.around(steering[0], 2)}")
         print()
 
         move_result = self.move_spaceship(object_positions,
                                           object_gravity, steering)
-        rendering = self.render(move_result[0])
+        rendering = np.array([self.render(objects) for objects in move_result[0]])
+        with h5py.File("datasets/SPACE/space_data_y.hdf5", "w") as f:
+            dset = f.create_dataset("Space_dataset_y", data=rendering)
+
         plt.subplot(122)
-        plt.imshow(rendering)
+        plt.imshow(rendering[0])
         plt.title(f"Movement: {move_result[1][0]}")
         plt.show()
+
 
     def move_spaceship(self, object_positions, object_gravity, steering):
         """
         """
-        distances = object_positions[1:] - object_positions[0]
-        weighted_distances = distances * object_gravity.T
-        direction = (steering + np.sum(weighted_distances, axis=0)) / 2
-        new_pos = np.around(object_positions[0] + direction)
-        new_dist = np.linalg.norm(object_positions[:1] - new_pos, axis=-1)
+        distances = object_positions[:, 1:] - np.expand_dims(object_positions[:, 0], 1)
+        weighted_distances = distances * np.expand_dims(object_gravity, -1)
+        direction = (steering + np.sum(weighted_distances, axis=1)) / 2
+        new_pos = np.around(object_positions[:, 0] + direction)
+        new_dist = np.linalg.norm(object_positions[:, 1:] -
+                                  np.expand_dims(new_pos, 1), axis=-1)
         # Round this to integers
-        while self.valid_new_pos(object_positions, new_pos, new_dist):
-            direction *= 0.5
-            new_pos = np.around(object_positions[0] + direction)
-            new_dist = np.linalg.norm(object_positions[1:] - new_pos, axis=-1)
-        object_positions[0] = new_pos
+        valid = self.valid_new_pos(object_positions, new_pos, new_dist)
+        while np.any(valid):
+            direction *= np.expand_dims(0.5 * valid + 1.0 * ~valid, 1)
+            new_pos = np.around(object_positions[:, 0] + direction)
+            new_dist = np.linalg.norm(object_positions[:, 1:] -
+                                      np.expand_dims(new_pos, 1), axis=-1)
+            valid = self.valid_new_pos(object_positions, new_pos, new_dist)
+        object_positions[:, 0] = new_pos
         direction = np.around(direction)
         return object_positions, direction
 
     def valid_new_pos(self, object_positions, new_pos, new_dist):
-        if np.any(new_dist < 1):
-            return True
-        if np.any(new_pos < 0.0):
-            return True
-        if np.any(new_pos > 5.0):
-            return True
-        return False
+        a = np.any(new_dist < 1, axis=1)
+        b = np.any(new_pos < 0.0, axis=1)
+        c = np.any(new_pos > 5.0, axis=1)
+        return a | b | c
 
     def render(self, object_positions):
         im = np.zeros((self.width * 10, self.height * 10, 3), dtype=float)
         for idx, pos in enumerate(object_positions):
-            print(idx, pos)
             if idx % 3 == 0:
                 rr, cc = skimage.draw.circle(
                     pos[0] * 10 + 5, pos[1] * 10 + 5, 5, im.shape)
@@ -131,12 +142,17 @@ class SpaceShapesGenerator():
         """ Generate positions of the objects in the grid."""
         positions = product(range(self.height), range(self.width))
         positions = np.array([i for i in positions])
-        position_idx = random.choice(len(positions), self.num_objects + 1,
-                                     replace=False, p=position_probs.flatten())
+        position_idx = []
+        for prob in position_probs:
+            position_idx.append(random.choice(len(positions),
+                                              self.num_objects + 1,
+                                              replace=False,
+                                              p=prob.flatten()))
+        position_idx = np.array(position_idx)
         object_positions = positions[position_idx]
         return object_positions
 
 
 if __name__ == "__main__":
     generator = SpaceShapesGenerator()
-    generator.generate_data(1)
+    generator.generate_data(1000)
