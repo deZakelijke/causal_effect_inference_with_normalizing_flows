9aee841145d32ba6d7df3f9de72550aa8f967a21
Micha de Groot <git@michadegroot.nl>
2021-02-22 13:47:14 +0100

    added post-processing scripts

--- data_test_script.py
+++ data_test_script.py
@@ -18,7 +18,8 @@ def collect_summary_dict(path):
     for item in summ[1:]:
         if item.summary.value[0].metadata.plugin_data.plugin_name == "scalars":
             tag = item.summary.value[0].tag
-            if not tag.startswith("metrics/test"):
+            if (not tag.startswith("metrics/test")) and\
+               (not tag.startswith("metrics/train/loss")):
                 continue
             data = tf.make_ndarray(item.summary.value[0].tensor)
             if tag in summ_dict:
@@ -33,16 +34,27 @@ def collect_summary_dict(path):
 
 def get_mean_and_std(summ_dict_list):
     for key in summ_dict_list[0]:
-        score = []
+        min_score = []
+        mean_score = []
+        last_score = []
         for summ_dict in summ_dict_list:
-            score.append(summ_dict[key][-1])
+            min_score.append(tf.reduce_min(summ_dict[key]))
+            mean_score.append(tf.reduce_mean(summ_dict[key]))
+            last_score.append(summ_dict[key][-1])
         print(key)
-        print(f"Mean: {tf.reduce_mean(score).numpy():.3f},\t"
-                f"Std: {tf.math.reduce_std(score).numpy():.3f}")
+        print("Minimum")
+        print(f"Mean: {tf.reduce_mean(min_score).numpy():.4f},\t"
+                f"Std: {tf.math.reduce_std(min_score).numpy():.4f}")
+        print("Mean")
+        print(f"Mean: {tf.reduce_mean(mean_score).numpy():.4f},\t"
+                f"Std: {tf.math.reduce_std(mean_score).numpy():.4f}")
+        print("Last")
+        print(f"Mean: {tf.reduce_mean(last_score).numpy():.4f},\t"
+                f"Std: {tf.math.reduce_std(last_score).numpy():.4f}")
 
 
-def process_data(log_path):
-    all_paths = get_all_summaries(log_path)
+def process_data(log_path, prefix):
+    all_paths = get_all_summaries(prefix + log_path)
     summ_dict_list = []
     for path in all_paths:
         summ_dict_list.append(collect_summary_dict(path))
@@ -52,6 +64,16 @@ def process_data(log_path):
     get_mean_and_std(summ_dict_list)
     print()
 
-log_paths = ["~/logs/CEVAE/SPACE/", "~/logs/CEVAE/TWINS"]
-for log_path in log_paths:
-    process_data(log_path)
+
+prefix = "/home/mdegroot/logs/"
+log_paths_space = ["TARNET/SPACE/", "CEVAE/SPACE/", "PlanarFlow/SPACE/",
+                   "RadialFlow/SPACE/", "SylvesterFlow/SPACE/",
+                   "NCF/AffineCoupling/SPACE/", "NCF/NLSCoupling/SPACE"]
+for log_path in log_paths_space:
+    process_data(log_path, prefix)
+
+log_paths_twins = ["TARNET/TWINS/", "CEVAE/TWINS/", "PlanarFlow/TWINS/",
+                   "RadialFlow/TWINS/", "SylvesterFlow/TWINS/",
+                   "NCF/AffineCoupling/TWINS/", "NCF/NLSCoupling/TWINS/"]
+for log_path in log_paths_twins:
+    process_data(log_path, prefix)
--- test_script_space.py
+++ test_script_space.py
@@ -1,18 +1,24 @@
 import subprocess
+import sys
 
 BATCH_SIZE = 16
 DATASET = "SPACE"
+DATASET_PATH = "datasets/SPACE/test_second_set_n_obj_"
 EPOCHS = 100
 EXP_NAME = "test_script_second_data_2_flows_fmaps_16"
-FEATURE_MAPS = 16
+EXP_NAME = "test_script_second_data_4_flows"
+FEATURE_MAPS = 32
 LEARNING_RATE = 0.0001
 LOG_STEPS=10
-N_FLOWS = 2
+N_FLOWS = 4
 LOG_DIR = "logs/"
 N_SAMPLES = 20
+MODE = "test"
+N_OBJ = 8
 
 
 MODELS = ["TARNET", "CEVAE", "PlanarFlow", "RadialFlow", "SylvesterFlow", "NCF"]
+# MODELS = ["SylvesterFlow"]
 FLOW_TYPE = ["AffineCoupling", "NLSCoupling"]
 
 for model in MODELS:
@@ -23,42 +29,51 @@ for model in MODELS:
     command = "python3.7 src/main.py "\
               f"--batch_size {BATCH_SIZE} "\
               f"--dataset {DATASET} "\
+              f"--debug "\
               f"--epochs {EPOCHS} "\
               f"--experiment_name {EXP_NAME} "\
               f"--feature_maps {FEATURE_MAPS} "\
               f"--flow_type AffineCoupling "\
               f"--learning_rate {LEARNING_RATE} "\
               f"--model {model} "\
+              f"--mode {MODE} "\
               f"--n_flows {N_FLOWS} "\
-              f"--n_samples {n_samples}"
+              f"--n_samples {N_SAMPLES} "\
+              f"--path_dataset {DATASET_PATH}{N_OBJ}_ "
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     stdout = str(stdout, 'utf-8')
     stderr = str(stderr, 'utf-8')
-    with open(f"{LOG_DIR}SPACE_stdout_{EXP_NAME}.txt", "a") as f:
+    with open(f"{LOG_DIR}SPACE_stdout_{EXP_NAME}_{MODE}_n_obj_{N_OBJ}.txt", "a") as f:
         f.write(f"\n\n{stdout}\n")
-    with open(f"{LOG_DIR}SPACE_stderr_{EXP_NAME}.txt", "a") as f:
+    with open(f"{LOG_DIR}SPACE_stderr_{EXP_NAME}_{MODE}_n_obj_{N_OBJ}.txt", "a") as f:
         f.write(f"{model}\n")
         f.write(f"\n\n{stderr}\n")
+
+# sys.exit(0)
 
 command = "python3.7 src/main.py "\
           f"--batch_size {BATCH_SIZE} "\
           f"--dataset {DATASET} "\
+          f"--debug "\
           f"--epochs {EPOCHS} "\
           f"--experiment_name {EXP_NAME} "\
           f"--feature_maps {FEATURE_MAPS} "\
           f"--flow_type NLSCoupling "\
           f"--learning_rate {LEARNING_RATE} "\
           f"--model {model} "\
-          f"--n_flows {N_FLOWS} "
+          f"--mode {MODE} "\
+          f"--n_flows {N_FLOWS} "\
+          f"--n_samples {N_SAMPLES} "\
+          f"--path_dataset {DATASET_PATH}{N_OBJ}_ "
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
 stdout = str(stdout, 'utf-8')
 stderr = str(stderr, 'utf-8')
-with open(f"{LOG_DIR}SPACE_stdout_{EXP_NAME}.txt", "a") as f:
+with open(f"{LOG_DIR}SPACE_stdout_{EXP_NAME}_{MODE}_n_obj_{N_OBJ}.txt", "a") as f:
     f.write(f"\n\n{stdout}\n")
-with open(f"{LOG_DIR}SPACE_stderr_{EXP_NAME}.txt", "a") as f:
+with open(f"{LOG_DIR}SPACE_stderr_{EXP_NAME}_{MODE}_n_obj_{N_OBJ}.txt", "a") as f:
     f.write(f"{model}\n")
     f.write(f"\n\n{stderr}\n")
